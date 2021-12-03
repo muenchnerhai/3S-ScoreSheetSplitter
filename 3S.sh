96e2028267ab0e2e0ced0c303ffe0af5b9ef0cad
Florian <muenchnerhai@gmail.com>
2021-12-03 10:16:51 +0100

    Update 3S.sh

--- 3S.sh
+++ 3S.sh
@@ -3,6 +3,7 @@ for file in /input/*
 do
 	if [ "${file}" == "*.pdf" ]
 	then
+		#TODO: read the f*cking documentation to get sh*t done: https://imagemagick.org/script/convert.php
 		echo ${file}
 	fi
 done
